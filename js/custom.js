3fa183faaa4348e938e669ab9fb3c5c530f3950f
debangshudassj <debangshu.das@sjinnovation.com>
2021-03-19 21:12:38 +0600

    first draft

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+
+// mobile menu jquery toggle function
+$(document).ready(function(){
+    $('#mobilemenu').click(function(){
+        $('#formobilemenu').slideToggle('slow');
+       
+    });
+});
